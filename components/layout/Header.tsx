61886580b1c7392c110983c9ce67b4fcacd53845
omar mohsen <o.mohsen@neoxero.com>
2025-08-06 22:42:44 +0300

    fix styles

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -15,15 +15,15 @@ export async function Header({ settings }: Props) {
 
   return (
       <header className={`bg-gray-50 pb-5`}>
-        <div className="flex items-center justify-between container py-5 flex-wrap gap-4">
-          <div className="flex items-center justify-start gap-2 text-text font-bold lg:min-w-72">
+        <div className="flex items-center justify-between container py-5 max-lg:flex-wrap gap-4">
+          <div className="flex items-center justify-start gap-2 text-text font-bold 2xl:min-w-72">
             <span className="flex-content-center text-white p-1.5 rounded-lg bg-primary">
               <Home size={18}/>
             </span>
             Logo
           </div>
           <SearchComponent />
-          <div className="flex items-center gap-1.5 ms-auto ">
+          <div className="flex items-center gap-1.5 ">
             <LocalSwitcher />
             <Sms className="text-xl cursor-pointer" />
             <Notification className="text-xl cursor-pointer" />
